e8e3ac2e81b7484af5e91d9488ef5f13fcf29dc2
coocoo1112 <46492679+coocoo1112@users.noreply.github.com>
2021-05-10 10:12:07 -0400

    POC python spectrogram

---
+++ Python Stuff/simultaneous_play_record.py
@@ -0,0 +1,156 @@
+import sounddevice as sd
+import numpy as np
+from scipy.signal import chirp
+import queue
+import threading
+import sys
+import shutil
+import math
+
+#global scaled
+buff_size = 20
+block_size = 256
+q = queue.Queue(maxsize=buff_size)
+event = threading.Event()
+gain = 10
+low, high = 1000, 3000
+block_duration = 50 #in milliseconds
+print(sd.query_devices())
+
+
+def callback(indata, outdata, frames, time, status):
+    try:
+        data = q.get_nowait()
+    except queue.Empty as e:
+        print('Buffer is empty: increase buffersize?', file=sys.stderr)
+        raise sd.CallbackAbort from e
+    if any(indata):
+        magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
+        magnitude *= gain / fftsize
+        line = (gradient[int(np.clip(x, 0, 1) * (len(gradient) - 1))]
+                for x in magnitude[low_bin:low_bin + columns])
+        print(*line, sep='', end='\x1b[0m\n')
+    else:
+        print('no input')
+    if len(data) < len(outdata):
+        print(len(data), len(outdata))
+        outdata[:len(data)] = data.reshape(len(data), 1)
+        outdata[len(data):] = np.zeros(((len(outdata) - len(data), 1)))
+        raise sd.CallbackStop
+    else:
+        outdata[:] = data.reshape((block_size, 1))
+    
+
+
+def out_callback(outdata, frames, time, status):
+    print(frames)
+    print(outdata.shape)
+    try:
+        data = q.get_nowait()
+    except queue.Empty as e:
+        print('Buffer is empty: increase buffersize?', file=sys.stderr)
+        raise sd.CallbackAbort from e
+    if len(data) < len(outdata):
+        print(len(data), len(outdata))
+        outdata[:len(data)] = data.reshape(len(data), 1)
+        outdata[len(data):] = np.zeros(((len(outdata) - len(data), 1)))
+        raise sd.CallbackStop
+    else:
+        outdata[:] = data.reshape((block_size, 1))
+
+def in_callback(indata, frames, time, status):
+        if status:
+            text = ' ' + str(status) + ' '
+            print('\x1b[34;40m', text.center(columns, '#'),
+                  '\x1b[0m', sep='')
+        if any(indata):
+            magnitude = np.abs(np.fft.rfft(indata[:, 0], n=fftsize))
+            magnitude *= gain / fftsize
+            line = (gradient[int(np.clip(x, 0, 1) * (len(gradient) - 1))]
+                    for x in magnitude[low_bin:low_bin + columns])
+            print(*line, sep='', end='\x1b[0m\n')
+        else:
+            print('no input')
+
+
+
+# try:
+#     with sd.Stream(device=(0,1), samplerate=fs, dtype='float32', latency='low', channels=(1,2), callback=callback, blocksize=0):
+#         input()
+# except KeyboardInterrupt:
+#     pass
+
+try:
+    columns, _ = shutil.get_terminal_size()
+except AttributeError:
+    columns = 80
+
+fs = 44100
+fs = int(sd.query_devices(1, 'input')['default_samplerate'])
+print(fs)
+T = 3
+t = np.linspace(0, T, T*fs)
+w = chirp(t, f0=1000, f1=3000, t1=T, method='linear').astype(np.float32)
+scaled = np.int16(w/np.max(np.abs(w)) * 32767) 
+scaled = w
+
+colors = 30, 34, 35, 91, 93, 97
+chars = ' :%#\t#%:'
+gradient = []
+
+for bg, fg in zip(colors, colors[1:]):
+    for char in chars:
+        if char == '\t':
+            bg, fg = fg, bg
+        else:
+            gradient.append('\x1b[{};{}m{}'.format(fg, bg + 10, char))
+
+
+delta_f = (high - low) / (columns - 1)
+fftsize = math.ceil(fs / delta_f)
+low_bin = math.floor(low / delta_f)   
+
+for _ in range(20):
+    data = scaled[:min(block_size, len(scaled))]
+    if len(data) == 0:
+        break
+    scaled = scaled[min(block_size, len(scaled)):]
+    q.put_nowait(data)
+
+
+with sd.Stream(device=(1,2), samplerate=fs, dtype='float32', latency='low', channels=(1,2), callback=callback, blocksize=block_size):
+    timeout = block_size * buff_size / fs
+    while len(data) != 0:
+        data = scaled[:min(block_size, len(scaled))]
+        scaled = scaled[min(block_size, len(scaled)):]
+        q.put(data, timeout=timeout)
+    event.wait()
+
+
+# with sd.InputStream(device=0, channels=1, callback=in_callback,
+#                         blocksize=block_size,#int(fs * block_duration / 1000),
+#                         samplerate=fs):
+#     while True:
+#         response = input()
+#         if response in ('', 'q', 'Q'):
+#             break
+
+
+
+
+
+#output stream working
+
+# try:
+#     stream = sd.OutputStream(device=1, samplerate=fs, dtype='float32', latency='low', channels=1, callback=out_callback, blocksize=block_size, finished_callback=event.set)
+#     with stream:
+#         print("starting")
+#         timeout = block_size * buff_size / fs
+#         while len(data) != 0:
+#             data = scaled[:min(block_size, len(scaled))]
+#             scaled = scaled[min(block_size, len(scaled)):]
+#             q.put(data, timeout=timeout)
+#         event.wait()
+
+# except KeyboardInterrupt:
+#         pass
